416c638213795f0e83e13c674a3060f38b1d664a
Guy Marriott <guy@chec.io>
2021-01-21 15:28:19 -0800

    Fetch cart cookies with js-cookie lib instead of relying on Commerce.js

--- src/cart/useCart.js
+++ src/cart/useCart.js
@@ -1,9 +1,14 @@
 import useSWR from 'swr';
 import useCommerceSwrFetcher from '../useCommerceSwrFetcher';
+import useActiveCartId from './useActiveCartId';
 
 export default function useCart(cartId) {
   const fetcher = useCommerceSwrFetcher();
-  const { data, error } = useSWR(['cart.retrieve', cartId], fetcher);
+  const defaultCartId = useActiveCartId();
+
+  // Rely on our own tracking of active cart ID above the logic in `cart.retrieve` itself. See comments in
+  // `useActiveCartId` for more details
+  const { data, error } = useSWR(['cart.retrieve', cartId || defaultCartId], fetcher);
 
   if (error) {
     throw error;
