0ea4ce6eeaa96779058f677339df7fa1034057e0
kvisca <kyle@viscadesign.ca>
2021-02-04 17:14:51 -0500

    Fixing issue that caused you to be unable to change shipping options to non existant options

--- src/checkout/useShippingOptions.js
+++ src/checkout/useShippingOptions.js
@@ -1,22 +1,43 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import useCommerce from '../useCommerce';
 import useCheckout from './useCheckout';
 
 export default function useShippingOptions(country, region) {
-  const [options, setOptions] = useState();
+  const [options, setOptions] = useState(null);
   const checkout = useCheckout();
   const commerce = useCommerce();
+  const previousArgs = useRef();
 
-  useEffect(async () => {
+  useEffect(() => {
     if (!commerce || !checkout || typeof country !== 'string' || country === '') {
       return;
     }
 
-    setOptions(await commerce.checkout.getShippingOptions(checkout.id, {
+    if (previousArgs.current) {
+      const prevCountry = previousArgs.current.country;
+      const prevRegion = previousArgs.current.region;
+
+      if (prevCountry !== country && prevRegion === region) {
+        setOptions(null);
+        previousArgs.current = { country, region };
+        return;
+      }
+    }
+
+    previousArgs.current = { country, region };
+
+    commerce.checkout.getShippingOptions(checkout.id, {
       country,
       region,
-    }));
-  }, [commerce, checkout && checkout.id, country, region]);
+    })
+      .then(setOptions)
+      .catch(({statusCode}) => {
+        // If doesnt exist, set to null.
+        if (statusCode === 404) {
+          setOptions(null);
+        }
+      })
+  }, [commerce, checkout, country, region]);
 
   return options;
 }
